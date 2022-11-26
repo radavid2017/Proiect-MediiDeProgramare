b12d1b86af99002dad4df5ea0819a1c7485ce964
David-Stelian Hoka <83161606+radavid2017@users.noreply.github.com>
2022-11-26 12:16:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proiect_MediiDeProgramare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
